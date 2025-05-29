c48545fdbd3aebf6fbaf112d2676cdededef62a6
aayanofficial5 <aayanofficial5@gmail.com>
2025-05-29 23:09:11 +0530

    update

---
+++ Week_1_Array/01_Prefix_Sum/Day1_E2.cpp
@@ -0,0 +1,37 @@
+// LeetCode 1480 : Running Sum of 1d Array
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> runningSum(vector<int> &nums)
+{
+  int sum = 0;
+  int n = nums.size();
+  for (int i = 0; i < n; i++)
+  {
+    sum += nums[i];
+    nums[i] = sum;
+  }
+
+  return nums;
+}
+
+int main()
+{
+  vector<int> nums = {1, 2, 3, 4};
+  cout << "Input Array: ";
+  // Print the input array
+  for (int i = 0; i < nums.size(); i++)
+  {
+    cout << nums[i] << " ";
+  }
+  cout << endl;
+  int sum = 0;
+  nums = runningSum(nums);
+  cout << "Running Sum: ";
+  for (int i = 0; i < nums.size(); i++)
+  {
+    cout << nums[i] << " ";
+  }
+  return 0;
+}
